745e2c136b72674a0b3ff712825ddeb38fd74fe5
Leopoldo Leonardi <leopoldo.leonardi@gmail.com>
2021-04-08 15:37:40 -0300

    Sallers page created and insertion of functions to the database

--- SalesWeb/Migrations/20210407140209_OtherEntities02.Designer.cs
+++ SalesWeb/Migrations/20210407140209_OtherEntities02.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SalesWeb.Models;
 
 namespace SalesWeb.Migrations
 {
     [DbContext(typeof(SalesWebContext))]
-    partial class SalesWebContextModelSnapshot : ModelSnapshot
+    [Migration("20210407140209_OtherEntities02")]
+    partial class OtherEntities02
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
